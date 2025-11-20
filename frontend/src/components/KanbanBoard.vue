eb09b15db28dd844240d634703e5ee6da11043dc
sawickaa <sawickaagata04@gmail.com>
2025-11-20 22:27:27 +0100

    dodawanie statusów w repo działa, do poprawki wygląd dodawania

--- frontend/src/components/KanbanBoard.vue
+++ frontend/src/components/KanbanBoard.vue
@@ -1,35 +1,41 @@
 <template>
-    <div v-if="!selectedRepo" class="text-center text-white mt-5">
-          <h4>Select a repository to view its issues</h4>
-        </div>
+  <div v-if="!selectedRepo" class="text-center text-white mt-5">
+    <h4>Select a repository to view its issues</h4>
+  </div>
 
-        <div v-else>
-          <h3 class="mb-4 text-white">
-            Issues for <span class="text-primary">{{ selectedRepo.name }}</span>
-          </h3>
-            <!--Kanban board-->
-            <div class="row row-cols-1 row-cols-lg-2 row-cols-xl-4 g-3">
-              <kanban-column
-                v-for="column in columns"
-                :key="column._id"
-                :column="column"
-                :issues-by-column="issuesByColumn"
-                :scroll-container="scrollContainer"
-                :groups="groups"
-                :on-drag-end="onDragEnd"
-                :open-issue="openIssue" 
-                :on-move-left="onMoveLeft"
-                :on-move-right="onMoveRight"
-                :delete-column="deleteColumn"
-                :edit-column="editColumn"
-                />
-            </div>
+  <div v-else>
+    <div class="d-flex justify-content-between align-items-center mb-4">
+      <h3 class="text-white">
+        Issues for <span class="text-primary">{{ selectedRepo.name }}</span>
+      </h3>
 
-        </div>
+      <button class="btn btn-success" @click="handleAddColumn">
+        Add column
+      </button>
+    </div>
+
+    <!-- Kanban board -->
+    <div class="row row-cols-1 row-cols-lg-2 row-cols-xl-4 g-3">
+      <kanban-column
+        v-for="column in columns"
+        :key="column._id"
+        :column="column"
+        :issues-by-column="issuesByColumn"
+        :scroll-container="scrollContainer"
+        :groups="groups"
+        :on-drag-end="onDragEnd"
+        :open-issue="openIssue"
+        :on-move-left="onMoveLeft"
+        :on-move-right="onMoveRight"
+        :delete-column="deleteColumn"
+        :edit-column="editColumn"
+      />
+    </div>
+
+  </div>
 </template>
 
 <script setup>
-// import KanbanColumn from './KanbanColumn.vue'
 import KanbanColumn from './KanbanColumn.vue'
 
 
@@ -45,7 +51,14 @@ defineProps({
   onMoveLeft: Function,  
   onMoveRight: Function,
   deleteColumn: Function,
-  editColumn: Function
+  editColumn: Function,
+  addColumn: Function
 })
 
+function handleAddColumn() {
+  const name = prompt("Enter column name:")
+  if (!name || !name.trim()) return
+
+  props.addColumn(props.selectedRepo.id, name.trim(), null)
+}
 </script>
